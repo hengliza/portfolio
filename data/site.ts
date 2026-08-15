68f83026b8c5a182ab2ea619c3a1fe5291a0ce26
Icesuza <hengliza81@gmail.com>
2026-08-15 08:43:07 +0700

    modified data

--- data/site.ts
+++ data/site.ts
@@ -1,3 +1,3 @@
 export const iconProps = { size: 16, strokeWidth: 1.5 };
 
-export const pageCopy = { email: "hengliza81@gmail.com", year: "2026" };
+export const pageCopy = { email: "hengliza81@gmail.com", telegram: "@Icesuza", year: "2026" };
